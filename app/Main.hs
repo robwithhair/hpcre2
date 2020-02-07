1eed4b0c2ddef847328fa2a5d5a20fe3fa787a1d
Rob Harrison <robwithhair@gmail.com>
2020-02-07 16:17:14 +0000

    converted to using UTF-8 ByteStrings because it suggests UTF8 support

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Text.Regex.PCRE2.Wrap
--- src/Text/Regex/PCRE2/Wrap.hs
+++ src/Text/Regex/PCRE2/Wrap.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 {-# LANGUAGE CApiFFI #-}
 
-module Text.Regex.PCRE2.Wrap(
-       compileRegex
+module Text.Regex.PCRE2.Wrap(compileRegex
+                           , compileRegexFromByte8String
 ) where
 
 
@@ -15,9 +15,15 @@ import qualified Data.Vector.Storable as V
 import qualified Data.Vector.Storable.Mutable as M
 import Data.Word
 import Data.Char
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as E
+import qualified Data.ByteString as B
 
 foreign import capi "pcre2.h value PCRE2_JIT_COMPLETE"
   c_PCRE2_JIT_COMPLETE :: CUInt
+
+foreign import capi "pcre2.h value PCRE2_ZERO_TERMINATED"
+  c_PCRE2_ZERO_TERMINATED :: CSize
 
 foreign import ccall "pcre2.h pcre2_compile_8"
   c_pcre2_compile :: Ptr CChar -> CSize -> CUInt -> Ptr CInt -> Ptr CSize -> Ptr () -> IO (Ptr Code)
@@ -40,18 +46,14 @@ type PCRE2ErrorOffset = Int
 
 data RegexCompilationError = CompilationError PCRE2ErrorCode PCRE2ErrorOffset | JITCompilationError PCRE2ErrorCode deriving Show
 
--- Convert a string to a vector of numbers of word8
-utf8Vector :: String -> V.Vector Word8
-utf8Vector = V.fromList . map (fromIntegral . ord)
-
 -- Get csize of vector
 cVectorSize :: V.Vector Word8 -> CSize
 cVectorSize = fromIntegral . V.length
 
 -- Compile a regex from a vector of 8 bit chars
-compileRegexFromVector :: V.Vector Word8 -> IO (Either RegexCompilationError (ForeignPtr Code))
-compileRegexFromVector regex = alloca $ \errorCode -> alloca $ \errorOffset -> do
-                       compiledRegex <- V.unsafeWith regex $ \regexPointer -> c_pcre2_compile (castPtr regexPointer) regexSize 0 errorCode errorOffset nullPtr
+compileRegexFromByte8String :: B.ByteString -> IO (Either RegexCompilationError (ForeignPtr Code))
+compileRegexFromByte8String regex = alloca $ \errorCode -> alloca $ \errorOffset -> do
+                       compiledRegex <- B.useAsCString regex $ \regexPointer -> c_pcre2_compile (castPtr regexPointer) c_PCRE2_ZERO_TERMINATED 0 errorCode errorOffset nullPtr
                        if compiledRegex == nullPtr then do
                           errorCodePeek <- peek errorCode
                           errorCodeOffset <- peek errorOffset
@@ -63,10 +65,8 @@ compileRegexFromVector regex = alloca $ \errorCode -> alloca $ \errorOffset -> d
                           else do
                              foreignRegex <- newForeignPtr c_pcre2_code_free compiledRegex
                              return $ Right foreignRegex
-                       where
-                       regexSize = cVectorSize regex
 
 -- Compile a regex from a String
-compileRegex :: String -> IO (Either RegexCompilationError (ForeignPtr Code))
-compileRegex = compileRegexFromVector . utf8Vector
+compileRegex :: T.Text -> IO (Either RegexCompilationError (ForeignPtr Code))
+compileRegex = compileRegexFromByte8String . E.encodeUtf8
 
